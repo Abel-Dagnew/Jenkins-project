63ade9267e8f5e8ec841954807deeaebbc80d49a
Abelvm <Abeltesfaye111@gmail.com>
2024-11-06 16:58:03 +0000

    update  docker and pipeline create image

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,69 +1,11 @@
 pipeline {
     agent any
-
-    environment {
-        TF_VAR_client_id       = credentials('ARM_CLIENT_ID')
-        TF_VAR_client_secret   = credentials('ARM_CLIENT_SECRET')
-        TF_VAR_tenant_id       = credentials('ARM_TENANT_ID')
-        TF_VAR_subscription_id = credentials('ARM_SUBSCRIPTION_ID')
-    }
-
-    stages {
-        stage('Terraform Init') {
-            steps {
-                script {
-                    echo 'Initializing Terraform...'
-                    // Check current PATH
-                    echo "Current PATH: ${env.PATH}" // Output the current PATH
-                    sh 'terraform init' // Make sure terraform is installed in PATH
-                }
-            }
-        }
-
-        stage('Terraform Plan') {
-            steps {
-                script {
-                    echo 'Running Terraform Plan...'
-                    // Provide feedback on the plan process
-                    sh 'terraform plan -target=module.Create_container_registry'
-                }
-            }
-        }
-
-        stage('Terraform Apply') {
-            steps {
-                script {
-                    echo 'Applying Terraform Configuration...'
-                    // Provide feedback before applying
-                    sh 'terraform apply -target=module.Create_container_registry -auto-approve'
-                }
-            }
-        }
-    }
-
-    post {
-        always {
-            echo 'Cleaning up workspace...'
-            deleteDir()
-        }
-        success {
-            echo 'Terraform Apply completed successfully!'
-        }
-        failure {
-            echo 'Terraform Apply failed. Please check the logs for errors.'
-        }
-    }
-}
-
-
-pipeline {
-    agent any
     environment {
         // Azure container registry details
         ACR_NAME = "abelregistryy"  // Your ACR name
         ACR_LOGIN_SERVER = "${ACR_NAME}.azurecr.io"  // ACR login server URL
         ACR_USERNAME = "abelregistryy"  // ACR username
-        ACR_PASSWORD = "jphu8GP6Wgg3TOyRSsoNYp6kxlKw+H5r6y4HnDVM9g+ACRDtGhSR"  // ACR password
+        ACR_PASSWORD = credentials('ACR_Pass')  // ACR password
         DOCKER_IMAGE_NAME = "Abelimage1st"  // Name of your Docker image
         GITHUB_REPO = "https://github.com/Abel-Dagnew/Jenkins-project.git"  // GitHub repository URL
     }
