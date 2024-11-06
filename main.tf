057acc704bd5ca337d7a2704fd834d6744c33d07
Abelvm <Abeltesfaye111@gmail.com>
2024-11-06 15:12:43 +0000

    update azure provider

--- main.tf
+++ main.tf
@@ -4,29 +4,19 @@ terraform {
       source  = "microsoft/azuredevops"
       version = "~> 0.1"
     }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"  # Specify the version you are using
-    }
+    
   }
 }
 
-
-
-
-
-module "Create_App_Service" {
-  
-  source = "./Create_App_Service"
- 
-     
-}
 module "Create_container_registry" {
   
   source = "./Create_container_registry"
- 
+  providers = {
+    azurerm = azurerm.azresourceprovider
+  }
      
 }
+
 
 
 
