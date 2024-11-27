ebb0ecc934037f5524a3a83dfb780f2862aaab00
Abelvm <Abeltesfaye111@gmail.com>
2024-11-27 12:28:50 +0000

    removed az install

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,28 +9,13 @@ pipeline {
         ACR_PASSWORD = credentials('ACR_Pass')  // ACR password stored in Jenkins credentials
         DOCKER_IMAGE_NAME = "mydocker-repo"  // Name of your Docker image
         GITHUB_REPO = "https://github.com/Abel-Dagnew/Jenkins-project.git"  // GitHub repository URL
+        // DOCKER_CREDENTIALS_ID = credentials('Docker_PAT')
         DOCKER_USERNAME = "abel13"
         AZURE_WEB_APP_NAME = "Abel-Container234" // Azure Web App name
         AZURE_RESOURCE_GROUP = "Abel-Container234_group" // Azure Resource Group where Web App resides
     }
 
     stages {
-        stage('Install Azure CLI') {
-            steps {
-                script {
-                    // Installing Azure CLI if not already available
-                    sh '''
-                    if ! command -v az &> /dev/null; then
-                        echo "Azure CLI not found, installing..."
-                        curl -sL https://aka.ms/InstallAzureCLIDeb | bash
-                    else
-                        echo "Azure CLI is already installed"
-                    fi
-                    '''
-                }
-            }
-        }
-
         stage('Login to Azure') {
             steps {
                 script {
@@ -43,8 +28,6 @@ pipeline {
                     ]) {
                         // Logging in to Azure using the Service Principal credentials
                         sh '''
-                            set -e  # Exit immediately if a command exits with a non-zero status
-                            echo "Logging in to Azure..."
                             az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
                             az account set --subscription $AZURE_SUBSCRIPTION_ID
                         '''
@@ -67,7 +50,6 @@ pipeline {
         stage('Build Docker Image') {
             steps {
                 script {
-                    echo "Building Docker image ${ACR_NAME}/${DOCKER_IMAGE_NAME}:latest"
                     sh "docker build -t ${ACR_NAME}/${DOCKER_IMAGE_NAME}:latest ."
                 }
             }
@@ -77,7 +59,6 @@ pipeline {
             steps {
                 script {
                     // List images to verify the image tag exists
-                    echo "Listing Docker images..."
                     sh 'docker images'
                 }
             }
@@ -86,7 +67,6 @@ pipeline {
         stage('Tag Docker Image') {
             steps {
                 script {
-                    echo "Tagging Docker image for ACR..."
                     // Tag the image for Azure Container Registry
                     sh "docker tag ${ACR_NAME}/${DOCKER_IMAGE_NAME}:latest ${ACR_LOGIN_SERVER}/${DOCKER_IMAGE_NAME}:latest"
                 }
@@ -96,7 +76,6 @@ pipeline {
         stage('Push Docker Image to ACR') {
             steps {
                 script {
-                    echo "Pushing Docker image to ACR..."
                     // Push the Docker image to Azure Container Registry
                     sh '''
                         docker push ${ACR_LOGIN_SERVER}/${DOCKER_IMAGE_NAME}:latest
@@ -104,11 +83,10 @@ pipeline {
                 }
             }
         }
-
+        
         stage('Pull Image to Web App') {
             steps {
                 script {
-                    echo "Configuring Azure Web App to use Docker image from ACR..."
                     // Configure Azure Web App to use the latest image from ACR
                     sh '''
                         az webapp config container set --name ${AZURE_WEB_APP_NAME} \
@@ -125,16 +103,7 @@ pipeline {
 
     post {
         always {
-            echo "Cleaning up workspace..."
             cleanWs()  // Clean up workspace after the job
-        }
-
-        success {
-            echo "Pipeline completed successfully!"
-        }
-
-        failure {
-            echo "Pipeline failed!"
         }
     }
 }
