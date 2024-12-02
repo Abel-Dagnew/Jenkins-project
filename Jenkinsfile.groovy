ab9b4fd118e071c7f8d69f9c8b1b939f791b3270
Abelvm <Abeltesfaye111@gmail.com>
2024-12-02 11:16:26 +0000

    az cli login removed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,26 +17,26 @@ pipeline {
 
     stages {
         
-        stage('Login to Azure') {
-            steps {
-                script {
-                    // Use withCredentials to inject the Azure Service Principal credentials
-                    withCredentials([ 
-                        string(credentialsId: 'ARM_CLIENT_ID', variable: 'AZURE_CLIENT_ID'),
-                        string(credentialsId: 'ARM_CLIENT_SECRET', variable: 'AZURE_CLIENT_SECRET'),
-                        string(credentialsId: 'ARM_TENANT_ID', variable: 'AZURE_TENANT_ID'),
-                        string(credentialsId: 'ARM_SUBSCRIPTION_ID', variable: 'AZURE_SUBSCRIPTION_ID')
-                    ]) {
+        // stage('Login to Azure') {
+        //     steps {
+        //         script {
+        //             // Use withCredentials to inject the Azure Service Principal credentials
+        //             withCredentials([ 
+        //                 string(credentialsId: 'ARM_CLIENT_ID', variable: 'AZURE_CLIENT_ID'),
+        //                 string(credentialsId: 'ARM_CLIENT_SECRET', variable: 'AZURE_CLIENT_SECRET'),
+        //                 string(credentialsId: 'ARM_TENANT_ID', variable: 'AZURE_TENANT_ID'),
+        //                 string(credentialsId: 'ARM_SUBSCRIPTION_ID', variable: 'AZURE_SUBSCRIPTION_ID')
+        //             ]) {
                         
-                        // Logging in to Azure using the Service Principal credentials
-                        sh '''
-                            az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
-                            az account set --subscription $AZURE_SUBSCRIPTION_ID
-                        '''
-                    }
-                }
-            }
-        }
+        //                 // Logging in to Azure using the Service Principal credentials
+        //                 sh '''
+        //                     az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
+        //                     az account set --subscription $AZURE_SUBSCRIPTION_ID
+        //                 '''
+        //             }
+        //         }
+        //     }
+        // }
 
         stage('Login to ACR') {
             steps {
