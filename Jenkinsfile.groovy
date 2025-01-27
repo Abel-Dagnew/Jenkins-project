97b45a360651243c78d8401540b4c66444e20d28
Abelvm <Abeltesfaye111@gmail.com>
2025-01-27 13:15:47 +0000

    added kube ceredntial

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -103,12 +103,21 @@ pipeline {
             steps {
                 script {
                     // Use kubeconfig from Jenkins credentials
-                    withKubeConfig([credentialsId: 'kubeconfig-credentials-id']) {
+                    withCredentials([ 
+                        string(credentialsId: 'ARM_CLIENT_ID', variable: 'AZURE_CLIENT_ID'),
+                        string(credentialsId: 'ARM_CLIENT_SECRET', variable: 'AZURE_CLIENT_SECRET'),
+                        string(credentialsId: 'ARM_TENANT_ID', variable: 'AZURE_TENANT_ID'),
+                        string(credentialsId: 'ARM_SUBSCRIPTION_ID', variable: 'AZURE_SUBSCRIPTION_ID')
+                    ]) {
                         sh '''
-                        kubectl apply -f kubernetes/deployment.yaml
-                        kubectl apply -f kubernetes/service.yaml
+                            az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
+                            az aks get-credentials --resource-group Abel-AKS-rg --name My-AKS-00 --admin
+                            kubectl apply -f kubernetes/deployment.yaml
+                            kubectl apply -f kubernetes/service.yaml
+
                         '''
-                    }
+}
+
                 }
             }
         }
