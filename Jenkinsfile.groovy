827faa8fce0f84b77929370cca222b3a2051d976
Abelvm <Abeltesfaye111@gmail.com>
2025-02-04 11:14:07 +0000

    shared lib added for login

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,6 @@
+@Library('shared-lib') _  // Replace with actual library name
+
+
 pipeline {
     agent any
 
@@ -19,20 +22,7 @@ pipeline {
 
         stage('Login to Azure') {
             steps {
-                script {
-                    withCredentials([ 
-                        string(credentialsId: 'ARM_CLIENT_ID', variable: 'AZURE_CLIENT_ID'),
-                        string(credentialsId: 'ARM_CLIENT_SECRET', variable: 'AZURE_CLIENT_SECRET'),
-                        string(credentialsId: 'ARM_TENANT_ID', variable: 'AZURE_TENANT_ID'),
-                        string(credentialsId: 'ARM_SUBSCRIPTION_ID', variable: 'AZURE_SUBSCRIPTION_ID')
-                    ]) {
-                        // Logging in to Azure using the Service Principal credentials
-                        sh '''
-                            az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
-                            az account set --subscription $AZURE_SUBSCRIPTION_ID
-                        '''
-                    }
-                }
+                azure-login()  // Call the shared library function
             }
         }
 
